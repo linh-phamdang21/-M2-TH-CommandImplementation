db52ebcb75797c221011f13d76b691c710a309d6
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-04-27 18:49:28 +0700

    Add files

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int sub(int firstOperand, int secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public int add(int firstOperand, int secondOperand) {
+        return firstOperand - secondOperand;
+    }
+}
